e8545ff16edc09c47e7a35b5e5a2da9acddd63f7
Nathan <nathan.gt.protic@gmail.com>
2022-07-15 22:48:30 -0400

    best push (clouds)
    Signed-off-by: Nathan <nathan.gt.protic@gmail.com>

--- levels/level.py
+++ levels/level.py
@@ -1,8 +1,16 @@
 import pygame
 from constants import WIDTH, HEIGHT
 from sprites import Player
+from sprites.BackgroundObject import BackgroundObject
 from sprites.enemy import Enemy
 import random
+
+CLOUDS = 10
+CLOUDSIMG = [
+    pygame.image.load("res/cloud-1.png"),
+    pygame.image.load("res/cloud-2.png"),
+    pygame.image.load("res/cloud-3.png"),
+]
 
 class Level:
     def __init__(self, num: int, screen: pygame.Surface, background: pygame.Surface) -> None:
@@ -14,9 +22,11 @@ class Level:
         playerImg = pygame.image.load("res/player.png")
         schnakeImg = pygame.image.load("res/shnake.png")
 
+
         # Setting up groups
         self.playerProjectileGroup = pygame.sprite.Group()
         self.enemyProjectileGroup = pygame.sprite.Group()
+        self.backgroundObjectsGroup = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
 
         # Setting up player
@@ -30,14 +40,27 @@ class Level:
 
             self.enemies.add(enemy)
             self.enemyProjectileGroup.add(enemy) # Enemy will count as a projectile cuz if it collides with player it will kill him
+        
+        for _ in range(CLOUDS):
+            self.backgroundObjectsGroup.add(self.generateCloud(True))
 
-
+    def generateCloud(self, randomY = False):
+            img = CLOUDSIMG[random.randint(0, len(CLOUDSIMG)-1)]
+            width = random.randint(int(WIDTH/10), int(WIDTH/2))
+            randomX = random.randint(0, WIDTH)
+            Y = random.randint(0 - HEIGHT/2, HEIGHT) if randomY else 0 - img.get_height()
+            # randomY = random.randint(0, HEIGHT)
+            speed =  pygame.Vector2(0, -10) #-(width / WIDTH)
+            return BackgroundObject(img, speed, width, center=(randomX,  Y))
+        
     def update(self, game, events, keys) -> None:
         self.pollInput(events, keys)
         self.enemies.update()
         self.enemyProjectileGroup.update()
         self.playerProjectileGroup.update()
         self.player.update(events, keys)
+        self.backgroundObjectsGroup.update()
+        if(len(self.backgroundObjectsGroup.sprites()) < CLOUDS): self.backgroundObjectsGroup.add(self.generateCloud())
 
         if not self.player.isAlive:
             game.switchState("MenuState")
@@ -45,6 +68,10 @@ class Level:
 
     def draw(self) -> None:
         self.screen.blit(self.background, (0, 0))
+
+        # Background elems
+        for sprite in self.backgroundObjectsGroup.sprites():
+            self.screen.blit(sprite.image, sprite.rect)
 
         # Enemies
         for sprite in self.enemies.sprites():
