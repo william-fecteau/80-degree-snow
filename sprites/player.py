9f7af562dd9cdcd5e736ad4da61ec16c3eb07253
Nathan <nathan.gt.protic@gmail.com>
2022-07-15 20:02:51 -0400

    ajout touches alternatives
    Signed-off-by: Nathan <nathan.gt.protic@gmail.com>

--- sprites/player.py
+++ sprites/player.py
@@ -54,20 +54,20 @@ class Player(pygame.sprite.Sprite):
             if event.type == E_PLAYER_SHOT_COOLDOWN:
                 self.canShoot = True
         
-        if keys[pygame.K_SPACE] and self.canShoot:
+        if (keys[pygame.K_SPACE] or keys[pygame.K_z]) and self.canShoot:
             self.canShoot = False
             self.shoot()
 
 
         # Move player
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.direction.y = 1
 
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.direction.x = 1
-        elif keys[pygame.K_LEFT]:
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.direction.x = -1
 
         self.rect.center += self.direction * self.speed
